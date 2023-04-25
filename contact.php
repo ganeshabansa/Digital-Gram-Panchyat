f5724561d95cc3e3d03a6c78e8016fcbbdeaa008
Amar359 <amartarte3@gmail.comm>
2023-04-25 23:55:52 +0530

    contact page changes

--- contact.php
+++ contact.php
@@ -46,226 +46,272 @@
 		/* .input_style:hover {
 		background-color: #45a049;
 		} */
+		section .container
+       {
 
-		.container
-		{
-			position: relative;
-			min-width: 1100px;
-			min-height: 550px;
-			display: flex;
-			z-index: 1000;
-		}
+    position: relative;
+    min-width: 1100px;
+     min-height: 550px;
+     display: flex;
+    z-index: 1000;         
+        }
 
-		.container .contactinfo
-		{
-			position: relative;
-			top: 40px;
-			width: 350px;
-			height: calc(100% - 80px);
-			background: #0f3959;
-			z-index: 1;
-			padding: 40px;
-			display: flex;
-			justify-content: space-between;
-		}
-		.container .contactinfo h2
-		{
-			color: #fff;
-			font-size: 24px;
-			font-weight: 500;
-		}
-		.container .contactinfo .info
-		{
-			position: relative;
-			margin: 20px 0;
+   section .container .contactinfo
+    {
 
-		}
-		.container .contactinfo .info li
-		{
-			position: relative;
-			list-style: none;
-			display: flex;
-			margin: 20px 0;
-			cursor: pointer;
-			align-items: flex-start;
-		}
-		.container .contactinfo .info li span:nth-child(1)
-
-		{
-		width: 30px;
-		min-width: 30px;
-		}
-		.container .contactinfo .info li span:nth-child(1) img
-		{
-			max-width: 100%;
-			filter: invert(1);
-			opacity: 0.5;
-		}
-		.container .contactinfo .info li span:nth-child(2)
-
-		{
-		color: #fff;
-		margin-left: 10px;
-		font-weight: 300;
-		opacity: 0.5;
-		}
-		.container .Contactform
-		{
-			position: absolute;
-			padding: 70px 50px;
-			background: #fff;
-			margin-left: 150px;
-			padding-left: 250px;
-			width: calc(100% - 150px);
-			height: 100%;
-			box-shadow: 0 50px 50px rgba(0,0,0,0.5);
-		}
-		.container .Contactform h2
-		{
-			color: #0f3959;
-			font-size: 24px;
-			font-weight: 500;
-		}
-		.container .Contactform .formBox
-		{
-			position: relative;
-			display: flex;
-			justify-content: space-between;
-			flex-wrap:wrap ;
-			padding: 30px;
-		}
-		.container .Contactform .formBox ,inputBox
-		{
-			position: relative;
-			margin: 0 0 35px 0;
-
-		}
-		.container .Contactform .formBox .inputBox.w50
-		{
-			width: 47%;
-		}
-		.container .Contactform .formBox .inputBox.w100
-		{
-			width: 100%;
-		}
-		.container .Contactform .formBox .inputBox input
-		.container .Contactform .formBox .inputBox textarea 
-		{
-			width: 100% limportant;
-			padding: 5px 0;
-			resize:none;
-			font-size: 18px;
-			font-weight: 300;
-			color: #333;
-			border: none;
-			border-bottom: 1px solid #777;
-			outline: none;
-
-		
-		}
-		.container .Contactform .formBox .inputBox textarea 
-		{
-		min-height: 130px;
-		}
-		.container .Contactform .formBox .inputBox span
-		{
-			position: absolute;
-			left: 0;
-			padding: 5px 0;
-			font-size: 18px;
-			font-weight: 300;
-			color: #333;
-			transition: 0.5s;
-			pointer-events: none;
+    position: relative;
+    top: 40px;
+    width: 350px;
+    height: calc(100% - 80px);
+    background: #0f3959;
+    z-index: 1;
+    padding: 40px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    box-shadow: 0 20px 20px rgba(0,0,0,0.2);
+                                            
+                                        }
 
 
-		}
-		.container .Contactform .formBox .inputBox input:focus~span
-		.container .Contactform .formBox .inputBox textarea:focus~span
-		.container .Contactform .formBox .inputBox input:valid~span
-		.container .Contactform .formBox .inputBox textarea:valid~span
-		{
-			transform: translateY(-20px);
-			font-size:12px;
-			font-weight: 400;
-			letter-spacing: 1px;
-			color: #ff568c;
+   section .container .contactinfo h2
+         
+   {
+    color: #fff;
+    font-size: 24px;
+    font-weight: 500;
+                       }
 
-		}
-		.container .Contactform .formBox .inputBox input[type="submit"]
-		{
-			position: relative;
-			cursor: pointer;
-			background: #0f3959;
-			color: #fff;
-			border: none;
-			max-width: 150px;
-			padding: 12px;
+  section .container .contactinfo .info
+       {
+    
+    position: relative;
+    margin: 20px 0;
+                     }
 
-		}
-		.container .Contactform .formBox .inputBox input[type="submit"]
-		{
-			background: #ff568c;
-		}
+   section .container .contactinfo .info li
+       {
+
+    position: relative;
+    list-style: none;
+    display: flex;
+    margin: 20px 0;
+    cursor: pointer;
+    align-items: flex-start;
+
+                }
+
+   section .container .contactinfo .info li span:nth-child(1)
+           {
+
+    width: 30px;
+   min-width: 30px;
+           }
+
+   section .container .contactinfo .info li span:nth-child(1) img
+         {
+    max-width: 100%;
+    filter: invert(1);
+    opacity: 0.5;
+            }
+
+    section .container .contactinfo .info li span:nth-child(2)
+        {
+
+    color: #fff;
+    margin-left: 10px;
+    font-weight: 300;
+    opacity: 0.5;
+               }
+
+   section .container .contactinfo .info li span:nth-child(1) img,
+   section .container .contactinfo .info li span:nth-child(2)
+          {
+
+    opacity: 1;
+         }
+
+   section .container .Contactform
+      {
+
+    position: absolute;
+    padding: 70px 50px;
+    background: #fff;
+    margin-left: 150px;
+    padding-left: 250px;
+    width: calc(100% - 150px);
+    height: 100%;
+    box-shadow: 0 50px 50px rgba(0,0,0,0.5);
+                                             }
+			    
+    section .container .Contactform h2
+                 {
+
+    color: #0f3959;
+    font-size: 24px;
+    font-weight: 500;
+                     }
+
+   section .container .Contactform .formBox
+           {
+
+    position: relative;
+    display: flex;
+    justify-content: space-between;
+    flex-wrap:wrap ;
+    padding-top: 30px;
+                  }
+
+   section .container .Contactform .formBox .inputBox
+           {
+
+    position: relative;
+    margin: 0 0 35px 0;
+                       }
+
+   section .container .Contactform .formBox .inputBox.w50
+           {
+
+    width: 47%;
+                   }
+
+   section .container .Contactform .formBox .inputBox.w100
+           {
+
+    width: 100%;
+                  }
+
+   section .container .Contactform .formBox .inputBox input,
+   section .container .Contactform .formBox .inputBox textarea 
+             {
+
+    width: 100% limportant ;
+    padding: 5px 0;
+    resize:none;
+    font-size: 18px;
+    font-weight: 300;
+    color: #333;
+    border: none;
+    border-bottom: 1px solid #777;
+    outline: none;
+                     }
+
+    section .container .Contactform .formBox .inputBox textarea 
+          {
+
+    min-height: 120px;
+                      }
+
+    section .container .Contactform .formBox .inputBox span
+         {
+
+    position: absolute;
+    left: 0;
+    padding: 5px 0;
+    font-size: 18px;
+    font-weight: 300;
+    color: #333;
+    transition: 0.5s;
+    pointer-events: none;
+                           }
+
+   section .container .Contactform .formBox .inputBox input:focus~span
+   section.container .Contactform .formBox .inputBox textarea:focus~span
+   section.container .Contactform .formBox .inputBox input:valid~span
+   section.container .Contactform .formBox .inputBox textarea:valid~span
+        {
+
+    transform: translateY(-20px);
+    font-size:12px;
+    font-weight: 400;
+    letter-spacing: 1px;
+    color: #ff568c;
+                       }
+
+    section.container .Contactform .formBox .inputBox input[type="submit"]
+               {
+
+    position: relative;
+    cursor: pointer;
+    background: #0f3959;
+    color: #fff;
+    border: none;
+    max-width: 150px;
+    padding: 12px;
+                       }
+
+    sec.container .Contactform .formBox .inputBox input[type="submit"]
+            {
+
+    background: #ff568c;
+                      }
+	
+
 	</style>
 </head>
 
-<body>
+
+
     <?php include('include/header.php');?>
 
     <!-- Your Code Goes here -->
-	<div class="container">
+	<body>
+        <section>
+        <div class="container">
 
-		<div class="contactinfo">
-			<h2>contact Info</h2>
-			<ul class="info">
-				<li>
-					<span><img src="./img/location.png"></span>
-					<span>41106 ganagapur town<br>
-						sambhaji nagar gangapur <br>
-						41106</span>
-				</li>
-				<li>
-					<span><img src="./img/email.png"></span>
-					<span>gramvikasyojana@gmail.com</span>
-				</li>
-				<li>
-					<span><img src="./img/phn.png"></span>
-					<span>310-386-1</span>
-				</li>
-			</ul>
-		</div>
-
-		<div class="Contactform">
-			<h2>send a message</h2>
-			<div class="formBox">
-				<div class="inputBox w50">
-					<input class="input_style" type="text" name="" placeholder="First Name" required>
-				</div>
-				
-				<div class="inputBox w50">
-					<input class="input_style" type="text" name="" placeholder="Second Name" required>
-				</div>
-				
-				<div class="inputBox w50">
-					<input class="input_style" type="text" name="" placeholder="Email Address" required>
-				</div>
-				
-				<div class="inputBox w50">
-					<input class="input_style" type="text" name="" placeholder="Phone Number" required>
-				</div>
-				
-				<div class="inputBox w100">
-					<textarea name="" placeholder="Write Your Message here..." required></textarea>
-				</div>
-				
-				<div class="inputBox w100">
-					<input class="input_style" type="Submit" value="send">
-				</div>
-			</div>
-		</div>
-	</div>
-
+            <div class="contactinfo">
+                <h2>contact Info</h2>
+                <ul class="info">
+                    <li>
+                        <span><img src="./img/location.png"></span>
+                        <span>41106 ganagapur town<br>
+                            sambhaji nagar gangapur <br>
+                            41106</span>
+                    </li>
+                    <li>
+                        <span><img src="./img/email.png"></span>
+                        <span>gramvikasyojana@gmail.com</span>
+                    </li>
+                    <li>
+                        <span><img src="./img/phn.png"></span>
+                        <span>310-386-1</span>
+                    </li>
+                </ul>
+            </div>
+    
+            <div class="Contactform">
+                <h2>send a message</h2>
+                <div class="formBox">
+                    <div class="inputBox w50">
+                        <input class="input_style" type="text" name="" placeholder="First Name" required>
+                    </div>
+                    
+                    <div class="inputBox w50">
+                        <input class="input_style" type="text" name="" placeholder="Second Name" required>
+                    </div>
+                    
+                    <div class="inputBox w50">
+                        <input class="input_style" type="text" name="" placeholder="Email Address" required>
+                    </div>
+                    
+                    <div class="inputBox w50">
+                        <input class="input_style" type="text" name="" placeholder="Phone Number" required>
+                    </div>
+                    
+                    <div class="inputBox w100">
+                        <textarea name="" placeholder="Write Your Message here..." required></textarea>
+                    </div>
+                    
+                    <div class="inputBox w100">
+                        <input class="input_style" type="Submit" value="send">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    </body>
+</html>
+	
 	<!-- You Stop Writing here -->
 	<?php include('include/footer.php');?>
 
