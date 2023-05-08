609bf01d1e45f6b3f52085430aeec0aa6ece6ee2
ganeshabansa <ganeshabansa@gmail.com>
2023-05-08 23:40:11 +0530

    That's fine

--- track_application.php
+++ track_application.php
@@ -18,9 +18,8 @@ if( strlen($_SESSION['login']) > 0 ) {
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	
     <!-- CSS Links -->
-	<!-- <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css"> -->
+	<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 	<link rel="stylesheet" href="css/header-footer.css">
-    <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <link rel="stylesheet" href="css/track-application.css">
 
     <!-- jQuery -->
@@ -44,82 +43,70 @@ if( strlen($_SESSION['login']) > 0 ) {
     />
 
     <style>
-      .container {
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    gap: 4rem;
-  }
+                
+        .container {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        gap: 4rem;
+        }
 
-  .box {
-    padding: 1rem;
-    border: 1px solid #ccc;
-    background-color: #f1f1f1;
-  }
+        .box {
+        padding: 1rem;
+        border: 1px solid #ccc;
+        background-color: #f1f1f1;
+        }
 
-  .box.text {
-    font-size: 1.2rem;
-    font-weight: bold;
-    text-align: center;
-    background: #9ebffb;
-  }
+        .box.text {
+        font-size: 1.2rem;
+        font-weight: bold;
+        text-align: center;
+        background: #9ebffb;
+        }
 
-  .box.input {
-    width: 300px;
-    height: 40px;
-    font-size: 1rem;
-    padding: 0.5rem;
-    background: #9ebffb;
-  }
+        .box.input {
+        width: 300px;
+        height: 40px;
+        font-size: 1rem;
+        padding: 0.5rem;
+        background: #9ebffb;
+        }
 
-  .box.button {
-    background-color: #007bff;
-    color: #fff;
-    border: none;
-    cursor: pointer;
-    font-size: 1.2rem;
-    padding: 0.5rem 1rem;
-    transition: all 0.2s ease-in-out;
-  }
+        .box.button {
+        background-color: #007bff;
+        color: #fff;
+        border: none;
+        cursor: pointer;
+        font-size: 1.2rem;
+        padding: 0.5rem 1rem;
+        transition: all 0.2s ease-in-out;
+        }
 
-  .box.button:hover {
-    background-color: #0069d9;
-  }
-        
+        .box.button:hover {
+        background-color: #0069d9;
+        }
+
     </style>
 
 	<title>Track Your Application</title>
 </head>
-     
- 
 
 <body>
     <?php include('include/header.php');?>
 
     <!-- Your Code start here --> 
-     <div class="outer-">
-    
-    <div class="container">
-      <div class="box text">Enter the Tracking number of your Application to track the Status</div>
-      <div class="box input"><input type="text" placeholder="Enter tracking number" style="width:100%;"></div>
-      <div class="box button" style="center;">Submit</div>
-    </div>
-    
-    </div>
-   
-    
 
-    
-    
- 
-        
-    
+    <div class="outer-">
+        <div class="container">
+            <div class="box text">Enter the Tracking number of your Application to track the Status</div>
+            <div class="box input"><input type="text" placeholder="Enter tracking number" style="width:100%;"></div>
+            <div class="box button">Submit</div>
+        </div>
+    </div>
     
     <!-- Your Code end here -->
     
 	<?php include('include/footer.php');?>
-
-	</script>
 
     <!-- javaScripts libraries-->
     <!-- <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script> -->
